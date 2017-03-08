953a8f72eb17f6434f5363afea47dddf2151426d
dsvistunov <d.svistuno0v1991@gmail.com>
2017-03-08 17:02:13 +0200

    commit

--- src/settings.py
+++ src/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'products_proj.urls'
+ROOT_URLCONF = 'src.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'products_proj.wsgi.application'
+WSGI_APPLICATION = 'src.wsgi.application'
 
 
 # Database
