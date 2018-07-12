116d9d1e18ca934c2b7036f6f0e576282f619299
dsvistunov <d.svistuno0v1991@gmail.com>
2018-07-12 16:04:26 +0300

    fixed routiong

--- product/urls.py
+++ product/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^products/(?P<slug>[\w-]+)/$', ProductListView.as_view(), name='productslist'),
     url(r'^products/$', CategoryListView.as_view(), name='categories'),
     url(r'^lastadded/', LastAdded.as_view(), name='lastadded'),
-    url(r'^', IndexView.as_view(), name='indexview'),
+    url(r'^$', IndexView.as_view(), name='indexview'),
 ]
